e33fa286ad6d8cd14a133f4b367dfc4aab25dba5
akash <aksofworks@gmail.com>
2026-08-31 13:56:36 +0530

    main file with a seperate function for the read write of different clients

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/akss4/redis-clone
+
+go 1.27.0
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+	defer func() {
+		fmt.Println("Closing connection...", conn.RemoteAddr())
+		conn.Close()
+	}()
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading data:", err)
+			break
+		}
+		message := string(buffer[:n])
+		fmt.Println("Received message:", message)
+
+		_, err = conn.Write([]byte("Message received: " + message))
+		if err != nil {
+			fmt.Println("Error writing data:", err)
+			break
+		}
+	}
+
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":6379")
+	fmt.Println("Server is listening on port 6379...")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConnection(conn)
+	}
+}
